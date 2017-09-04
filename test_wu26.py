ce95901694a1dad335c40dbaa199a10294fbf1b2
Bert Bristow <bert.bristow@luup.com>
2017-09-04 13:11:05 +0100

    added Remote

--- test_wu26.py
+++ test_wu26.py
@@ -6,6 +6,10 @@ class TestSelenium(unittest.TestCase):
     #test case!
     def test_browse_google(self):
         driver = webdriver.Chrome()
+        # driver = webdriver.Remote(
+        #     command_executor='http://127.0.0.1:4444/wd/hub',
+        #     desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True})
+
         driver.get('http://wu-web.s3-website-eu-west-1.amazonaws.com/')
         print driver.title
         assert driver.title == "World United"
