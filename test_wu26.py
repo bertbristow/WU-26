b78aa65d82394c1f40dbc2ce26857ed327c14865
Bert Bristow <bert.bristow@luup.com>
2017-09-08 11:08:37 +0100

    test 5

--- test_wu26.py
+++ test_wu26.py
@@ -6,7 +6,7 @@ class TestSelenium(unittest.TestCase):
     #test case!!!!
     def test_browse_google(self):
         driver = webdriver.Chrome()
-        # driver = webdriver.Remote(
+        # driver = webdriver.Remote()
         #     command_executor='http://127.0.0.1:4444/wd/hub',
         #     desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True})
 
