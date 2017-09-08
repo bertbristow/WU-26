524e42ff24e4224b50890fc27f75f2db09d9d057
Bert Bristow <bert.bristow@luup.com>
2017-09-08 11:46:04 +0100

    test 6

--- test_wu26.py
+++ test_wu26.py
@@ -10,7 +10,7 @@ class TestSelenium(unittest.TestCase):
         #     command_executor='http://127.0.0.1:4444/wd/hub',
         #     desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True})
 
-        driver.get('http://wu-web.s3-website-eu-west-1.amazonaws.com/')
+        driver.get('https://dev.worldunited.com/signup/register')
         print driver.title
         assert driver.title == "World United"
 
@@ -18,7 +18,7 @@ class TestSelenium(unittest.TestCase):
         display = Display(visible=0, size=(800, 600))
         display.start()
         driver = webdriver.Firefox()
-        driver.get('http://wu-web.s3-website-eu-west-1.amazonaws.com/')
+        driver.get('https://dev.worldunited.com/signup/register')
         print driver.title
         assert driver.title == "World United"
 
