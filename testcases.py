864fcbb10164ce2602ba7ea2af309165083b7656
Bert Bristow <bert.bristow@luup.com>
2017-09-11 22:28:45 +0100

    Test1

--- testcases.py
+++ testcases.py
@@ -1,11 +1,11 @@
 
+from __future__ import print_function
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-'''from pyvirtualdisplay import Display
-'''
+from pyvirtualdisplay import Display
 
 class TestSelenium(unittest.TestCase):
     #test case!!!!
@@ -28,22 +28,35 @@ class TestSelenium(unittest.TestCase):
         driver.quit()
 
     def test_signup_failure(self):
-        email = 'test@test.com'
-        password = 'testpassword'
+        email = 'test1@test.com'
+        password = 'testpassword1'
 
-        driver = webdriver.Firefox()
+        driver = webdriver.Chrome()
         driver.get('https://d2qa3wj7w00hfu.cloudfront.net/signup')
-        driver.find_element_by_xpath('//input[@name="emailAddress"]').send_keys(email)
-        driver.find_element_by_xpath('//input[@name="password"]').send_keys(password)
-        driver.find_element_by_xpath('//input[@name="confirmPassword"]').send_keys(password)
-        driver.find_element_by_xpath('//input[@type="checkbox"]').click()
-        driver.find_element_by_css_selector('button.ui.fluid.primary.button').click()
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/form/div/div/div[2]/div[1]/input').send_keys(email)
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/form/div/div/div[3]/div[1]/div[1]/input').send_keys(password)
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/form/div/div/div[4]/div[1]/div/input').send_keys(password)
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/form/div/div/div[5]/div/label').click()
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div[4]/button[1]').click()
+        driver.implicitly_wait(10)
+
+
+        # https://gist.github.com/huangzhichong/3284966
+
+        # Wait for page to load
+        # driver.implicitly_wait(10)
+
+        # find element by xpath, insert text
+        # find element by xpath, click
         
         try:
-            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR , 'div.ui.error.message')))
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH , '//*[@id="root"]/div/div/div/div/form/div/div/div[6]/div/div[1]')))
         finally:
-            assert element.text == 'Signup failed, please try again later'
+            assert element.text.startswith('Signup failed')
             driver.quit()
+
+if __name__=='__main__':
+    unittest.main()
 
 
 if __name__=='__main__':
